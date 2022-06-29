fea6c87b119c0d49dd1e3f30b4218c66fdc118ad
jamuna0930 <105413735+jamuna0930@users.noreply.github.com>
2022-06-29 06:31:03 -0700

    Add files via upload

---
+++ array.java
@@ -0,0 +1,31 @@
+package operator;
+
+public class array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int a[]= {5};
+      int b[]= {10};
+      int c[]= {15};
+      int d[]= {20};
+      int [] []e = {{5},{15}};
+      int []f[]  = {{23},{15}};
+      int [] []g ={{56},{65}};
+      
+      System.out.println(" The values is " + a[0]);
+      
+      System.out.println(" The values is " + b[0]);
+      
+      System.out.println(" The values is " + c[0]);
+      
+      System.out.println(" The values is " + d[0]);
+      
+      System.out.println(" The values is " + e[0][0]);
+      
+      System.out.println(" The values is " + f[1][0]);
+      
+      System.out.println(" The values is " + g[1][0]);
+      
+	}
+
+}
